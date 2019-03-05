2c026d507aa8c6a1e6b89950c1f8575992f12a88
NMoore42 <nicholas.moore5@icloud.com>
2019-03-05 16:06:57 -0600

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,9 @@
+#I received direction for the below soultion from https://stackoverflow.com/questions/35778202/how-can-i-sort-an-array-of-strings-based-on-a-non-standard-alphabet.  This was my first exposure to the .tr method and it seems to be something that could be very versatile in future labs.
+
 def alphabetize(arr)
-  # code here
+  alphatbet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  ascii = "@-\\"
+  new_arr = arr.sort_by do |string|
+    string.tr(alphatbet, ascii)
+  end
 end
