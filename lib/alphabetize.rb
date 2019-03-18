a92c63de5cdf65c2dca7accf4d03761ea004097a
Diana Utan <dia_utzi@yahoo.com>
2019-03-18 21:03:55 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,8 @@
 def alphabetize(arr)
   # code here
+  esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  ascii = "@-\\"
+  arr.sort_by do |string|
+  string.tr(esperanto_alphabet, ascii)
+end
 end
